d02092f2942793ac6eafeab4a39466a050f20038
Shiva <sivar15racer@gmail.com>
2024-10-20 17:38:55 +0530

    eslint handled

--- master-admin-ui-service/src/app/pages/login/page.tsx
+++ master-admin-ui-service/src/app/pages/login/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GoogleLoginPage from '@/app/components/singleUse/GoogleLoginPage';
 
 const loginPage = () => {
+    console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID, "HERE")
     return (
         <GoogleLoginPage />
     )
